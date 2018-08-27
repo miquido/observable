42f40e44e3e1ab764af8d135eb2219f436a8f14a
k911 <konradobal@gmail.com>
2018-08-27 13:33:53 +0200

    test(phpunit): Add example test
    Example test is needed due to check if CI is configured properly

---
+++ tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Miquido\Observable\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestExample extends TestCase
+{
+
+    public function testIfUnitTestWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
